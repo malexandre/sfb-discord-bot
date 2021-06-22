1c1428b64f686a9a866dcbbb09eb9b691234faac
Marc Alexandre <github@malexandre.fr>
2021-06-22 17:25:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Discord bot to draw 7 random characters from SFB
+
+If you want to use it yourself, you can host it on Heroku. Remember to setup your bot token as an env var.
---
+++ index.ts
@@ -0,0 +1,39 @@
+import { Client, MessageEmbed, Message } from "discord.js";
+import config from "./config.json";
+
+const TOKEN = process.env.TOKEN;
+
+function shuffle<T>(array: T[]) {
+  var currentIndex = array.length, randomIndex: number;
+
+  while (0 !== currentIndex) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+function onMessage(msg: Message) {
+  if (msg.content === `${config.prefix}sfb`) {
+    const response = new MessageEmbed()
+    response.setTitle("Tirage de ta partie :")
+    response.setDescription(`${msg.author.tag} sera le ${Math.random() < 0.5 ? "premier" : "second" } joueur.`)
+    response.addField("Champions disponibles :", shuffle(config.champions).slice(0, 7).join("\n"))
+
+    msg.channel.send(response)
+  }
+}
+
+const BOT = new Client();
+
+BOT.on('ready', () => {
+  console.info(`Logged in as ${BOT.user.tag}!`);
+});
+BOT.on('message', onMessage);
+
+BOT.login(TOKEN);
